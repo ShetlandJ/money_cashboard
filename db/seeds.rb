873ef92db52b397062cf2cf11f1c71936592cff7
James Stewart <james.stewart.06@aberdeen.ac.uk>
2017-10-15 13:54:30 +0100

    updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,22 +27,6 @@ tag11 = Tag.new({'t_name' => 'Drinks'})
 tag12 = Tag.new({'t_name' => 'Video Games'})
 tag13 = Tag.new({'t_name' => 'Days out'})
 
-tag1.save()
-tag2.save()
-tag3.save()
-tag4.save()
-tag5.save()
-tag6.save()
-tag7.save()
-tag8.save()
-tag9.save()
-tag10.save()
-tag11.save()
-tag12.save()
-tag13.save()
-
-
-
 vendor1 = Vendor.new({'name' => 'Glasgow City Council'})
 vendor2 = Vendor.new({'name' => 'Solarplicity'})
 vendor3 = Vendor.new({'name' => 'Scottish Gas'})
@@ -59,6 +43,19 @@ vendor13 = Vendor.new({'name' => 'Super Bario'})
 vendor14 = Vendor.new({'name' => 'Fault & Blame'})
 vendor15 = Vendor.new({'name' => 'Steam (Valve)'})
 
+tag1.save()
+tag2.save()
+tag3.save()
+tag4.save()
+tag5.save()
+tag6.save()
+tag7.save()
+tag8.save()
+tag9.save()
+tag10.save()
+tag11.save()
+tag12.save()
+tag13.save()
 vendor1.save()
 vendor2.save()
 vendor3.save()
@@ -78,52 +75,121 @@ vendor15.save()
 transaction1 = Transaction.new({
   'vendor_id' => vendor1.id,
   'tag_id' => tag1.id,
-  'amount' => 150,
+  'amount' => 120,
   'transaction_date' => '01-12-2017'
 }
 )
 
-transaction1.save()
-
 transaction2 = Transaction.new({
   'vendor_id' => vendor2.id,
   'tag_id' => tag2.id,
-  'amount' => 15,
+  'amount' => 50,
+  'transaction_date' => '01-12-2017'
+}
+)
+
+transaction3 = Transaction.new({
+  'vendor_id' => vendor4.id,
+  'tag_id' => tag4.id,
+  'amount' => 20,
+  'transaction_date' => '01-12-2017'
+}
+)
+
+transaction4 = Transaction.new({
+  'vendor_id' => vendor5.id,
+  'tag_id' => tag5.id,
+  'amount' => 10,
+  'transaction_date' => '01-12-2017'
+}
+)
+
+transaction5 = Transaction.new({
+  'vendor_id' => vendor6.id,
+  'tag_id' => tag6.id,
+  'amount' => 450,
+  'transaction_date' => '03-12-2017'
+}
+)
+
+transaction6 = Transaction.new({
+  'vendor_id' => vendor7.id,
+  'tag_id' => tag7.id,
+  'amount' => 20,
+  'transaction_date' => '01-12-2017'
+}
+)
+
+transaction7 = Transaction.new({
+  'vendor_id' => vendor8.id,
+  'tag_id' => tag8.id,
+  'amount' => 9,
+  'transaction_date' => '02-12-2017'
+}
+)
+
+transaction8 = Transaction.new({
+  'vendor_id' => vendor10.id,
+  'tag_id' => tag10.id,
+  'amount' => 31,
+  'transaction_date' => '07-12-2017'
+}
+)
+
+transaction9 = Transaction.new({
+  'vendor_id' => vendor10.id,
+  'tag_id' => tag10.id,
+  'amount' => 25,
   'transaction_date' => '14-12-2017'
 }
 )
 
+transaction10 = Transaction.new({
+  'vendor_id' => vendor11.id,
+  'tag_id' => tag11.id,
+  'amount' => 100,
+  'transaction_date' => '05-12-2017'
+}
+)
+
+transaction11 = Transaction.new({
+  'vendor_id' => vendor11.id,
+  'tag_id' => tag11.id,
+  'amount' => 120,
+  'transaction_date' => '19-12-2017'
+}
+)
+
+transaction12 = Transaction.new({
+  'vendor_id' => vendor13.id,
+  'tag_id' => tag13.id,
+  'amount' => 5,
+  'transaction_date' => '07-12-2017'
+}
+)
+
+transaction13 = Transaction.new({
+  'vendor_id' => vendor13.id,
+  'tag_id' => tag13.id,
+  'amount' => 9,
+  'transaction_date' => '14-12-2017'
+}
+)
+
+transaction1.save()
 transaction2.save()
-
-transaction3 = Transaction.new({
-  'vendor_id' => vendor3.id,
-  'tag_id' => tag3.id,
-  'amount' => 80,
-  'transaction_date' => '15-12-2017'
-}
-)
 transaction3.save()
-
-
-transaction4 = Transaction.new({
-  'vendor_id' => vendor2.id,
-  'tag_id' => tag2.id,
-  'amount' => 40,
-  'transaction_date' => '23-12-2017'
-}
-)
 transaction4.save()
-
-
-transaction5 = Transaction.new({
-  'vendor_id' => vendor3.id,
-  'tag_id' => tag3.id,
-  'amount' => 400,
-  'transaction_date' => '20-12-2017'
-}
-)
-
 transaction5.save()
+transaction6.save()
+transaction7.save()
+transaction8.save()
+transaction9.save()
+transaction10.save()
+transaction11.save()
+transaction12.save()
+transaction13.save()
+
 
 
 pry.binding
